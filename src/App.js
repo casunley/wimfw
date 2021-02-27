d51b926e21fde10a67284ea306896b08ab2d5513
Curtis <casunley@uncg.edu>
2021-02-27 11:27:21 -0500

    Change App.js structure to render component to virtual dom, fix link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://https://www.youtube.com/watch?v=XtAhISkoJZc"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello Laurie :)
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://www.youtube.com/watch?v=XtAhISkoJZc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hello Laurie :)
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
+import ReactDOM, {render} from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
