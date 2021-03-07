bc8c41eecb8a2b81f4cfd09764d65b6c655c4ba8
Curtis <casunley@uncg.edu>
2021-03-07 11:18:56 -0500

    Separate buttons into component

---
+++ src/components/AdviceButton.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class AdviceButton extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
